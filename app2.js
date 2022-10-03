5b33810bb2f78a554ee338daf7b0b02256f9851a
shafik720 <shafikrasel5@gmail.com>
2022-10-03 20:54:08 +0600

    sdfsdf

--- app2.js
+++ app2.js
@@ -7,6 +7,8 @@ addNoteDesc = popupBox.querySelector('.popup-details textarea'),
 addButton = popupBox.querySelector('.popup-details button')
 
 
+let notes = JSON.parse(localStorage.getItem('notes') || '[]')
+
 addBox.addEventListener('click',()=>{
     popupBox.classList.add('show');
 })
@@ -17,9 +19,14 @@ closeIcon.addEventListener('click',()=>{
     addNoteDesc.value = '';
 })
 
-addButton.addEventListener('click',()=>{
+addButton.addEventListener('click',()=>{    
     if(addNoteTitle.value || addNoteDesc.value){
-        console.log('ok');
+        let dateObj = new Date();
+        let day = dateObj.getDate();
+        let month = dateObj.getMonth();
+        let year = dateObj.getFullYear();
+        let noteObj = {title:addNoteTitle.value, desc:addNoteDesc.value, date:`${day} ${month} ${year} `};
+        
     }
     
 })
