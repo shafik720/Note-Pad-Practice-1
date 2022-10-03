718dfe5619e310f4bb3479358404e213b613321e
shafik720 <shafikrasel5@gmail.com>
2022-10-03 20:48:31 +0600

    sdfsdf

--- app2.js
+++ app2.js
@@ -1,7 +1,10 @@
 
 const addBox = document.querySelector('.add-box'),
 popupBox = document.querySelector('.pop-box'),
-closeIcon = popupBox.querySelector('.popup-header i')
+closeIcon = popupBox.querySelector('.popup-header i'),
+addNoteTitle = popupBox.querySelector('.popup-details input'),
+addNoteDesc = popupBox.querySelector('.popup-details textarea'),
+addButton = popupBox.querySelector('.popup-details button')
 
 
 addBox.addEventListener('click',()=>{
@@ -10,4 +13,13 @@ addBox.addEventListener('click',()=>{
 
 closeIcon.addEventListener('click',()=>{
     popupBox.classList.remove('show');
+    addNoteTitle.value = '';
+    addNoteDesc.value = '';
+})
+
+addButton.addEventListener('click',()=>{
+    if(addNoteTitle.value || addNoteDesc.value){
+        console.log('ok');
+    }
+    
 })
